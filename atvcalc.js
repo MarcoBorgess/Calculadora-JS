303b9513c1654a77a5aa083e46d1ff5d307636fb
zMaarrco <gamesmrco@gmail.com>
2020-09-26 17:07:50 -0300

    quase lá

--- atvcalc.js
+++ atvcalc.js
@@ -1,14 +1,27 @@
 
-var valor;
-var resultado;
-
-function botao(num) {
-    valor = document.calc.visor.value += num;
+function addChar(input, character) {
+    if(input.value == null || input.value == "0")
+    input.value = character
+    else
+    input.value += character
 }
-function limpar(){
-    document.calc.visor.value = '';
+function deleteChar(input) {
+    input.value = input.value.substring(0, input.value.length - 1)
 }
-function calcular(){
-    resultado = eval(valor);
-    document.calc.visor.value = resultado;
+var val = 0.0;
+function percent(input) {
+val = input.value;
+input.value = input.value + "%";
+}
+function changeSign(input) {
+if(input.value.substring(0, 1) == "-")
+input.value = input.value.substring(1, input.value.length)
+else
+input.value = "-" + input.value
+}
+function compute(form) {
+    form.display.value = eval(form.display.value);
+}
+function square(form) {
+    form.display.value = eval(form.display.value) * eval(form.display.value)
 }
